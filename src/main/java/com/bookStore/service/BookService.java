7693d4d4c9f390a0d696a13ada554106ae5474c4
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-11-03 17:13:32 +0600

    book store project edited

---
+++ src/main/java/com/bookStore/service/BookService.java
@@ -0,0 +1,17 @@
+package com.bookStore.service;
+
+import com.bookStore.entity.Book;
+import com.bookStore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+}
+
